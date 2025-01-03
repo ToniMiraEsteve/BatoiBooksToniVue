4ca7ccd275159564ba89500fdc03f48afd9e4ab9
Toni Mira Esteve <tonimiraesteve@gmail.com>
2025-01-03 18:30:38 +0100

    eliminacion de nodes_modules

--- BatoiBooksVue/src/router/index.js
+++ BatoiBooksVue/src/router/index.js
@@ -1,23 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import BooksList from '../components/BooksList.vue';
+import AddBook from '../components/AddBook.vue';
+import AppAbout from '../components/AppAbout.vue';
+
+const routes = [
+  { path: '/', component: BooksList },
+  { path: '/add', component: AddBook },
+  { path: '/about', component: AppAbout },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
